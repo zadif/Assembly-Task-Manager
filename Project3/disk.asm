34e2c9d834b47acb81508f9749a28756093fe185
Zadif <zadifmustafa93@gmail.com>
2025-05-06 14:52:57 +0500

    Showing for all available disks

--- Project3/disk.asm
+++ Project3/disk.asm
@@ -1,4 +1,4 @@
-include functionHeaders.inc
+INCLUDE functionHeaders.inc
 .386
 .model flat, stdcall
 .stack 4096
@@ -9,34 +9,42 @@ GetDiskFreeSpaceExA PROTO :PTR BYTE, :PTR QWORD, :PTR QWORD, :PTR QWORD
 .data
 drivePath db "A:\", 0
 msg1 db "Available Drives and Storage Info (in GB):", 0Dh, 0Ah, 0
-msgTotal db " Total: ", 0
-msgFree db " Free: ", 0
+msgTotal db "  Total: ", 0
+msgFree db "  Free: ", 0
+msgError db "  N/A (Unable to retrieve)", 0
 szCRLF db 0Dh, 0Ah, 0
 
 driveMask DWORD ?
 totalSpace QWORD ?
 freeSpace QWORD ?
 unused QWORD ?
+bytesPerGB QWORD 1073741824 ; 1 GB = 2^30 bytes
 
 .code
-
 diskInfo PROC
     pushad
+    ; Display header
     mov edx, OFFSET msg1
     call WriteString
 
+    ; Get logical drives bitmask
     INVOKE GetLogicalDrives
     mov driveMask, eax
 
-    mov ecx, 0              ; Drive letter index (A-Z)
+    ; Check each drive (A to Z)
+    mov ecx, 0              ; Drive letter index (0 = A, 1 = B, etc.)
     mov ebx, driveMask      ; Bitmask of available drives
 
 check_drive:
     cmp ecx, 26
     jge done
 
+    ; Check if drive exists (bit set in mask)
     test ebx, 1
     jz skip_drive
+
+    ; Preserve ECX (loop counter)
+    push ecx
 
     ; Set current drive letter in drivePath (e.g., C:\)
     mov al, 'A'
@@ -53,27 +61,50 @@ check_drive:
 
     ; Call GetDiskFreeSpaceExA
     INVOKE GetDiskFreeSpaceExA, ADDR drivePath, ADDR unused, ADDR totalSpace, ADDR freeSpace
+    cmp eax, 0              ; Check if call succeeded
+    je error_space
 
-    ; Total GB = totalSpace / 1GB
-    mov eax, dword ptr totalSpace
-    mov edx, dword ptr totalSpace + 4
-    mov ecx, 1073741824         ; 1 GB in bytes
-    call Div64
+    ; Display total space
     mov edx, OFFSET msgTotal
     call WriteString
+    mov eax, DWORD PTR totalSpace
+    mov edx, DWORD PTR totalSpace + 4
+    push edx
+    push eax
+    call DivideByGB
     call WriteDec
     call CrLf
 
-    ; Free GB = freeSpace / 1GB
-    mov eax, dword ptr freeSpace
-    mov edx, dword ptr freeSpace + 4
-    mov ecx, 1073741824
-    call Div64
+    ; Display free space
     mov edx, OFFSET msgFree
     call WriteString
+    mov eax, DWORD PTR freeSpace
+    mov edx, DWORD PTR freeSpace + 4
+    push edx
+    push eax
+    call DivideByGB
     call WriteDec
     call CrLf
     call CrLf
+    jmp restore_ecx
+
+error_space:
+    ; Display error message for failed disk space query
+    mov edx, OFFSET msgTotal
+    call WriteString
+    mov edx, OFFSET msgError
+    call WriteString
+    call CrLf
+    mov edx, OFFSET msgFree
+    call WriteString
+    mov edx, OFFSET msgError
+    call WriteString
+    call CrLf
+    call CrLf
+
+restore_ecx:
+    ; Restore ECX (loop counter)
+    pop ecx
 
 skip_drive:
     shr ebx, 1              ; Shift mask to next drive
@@ -85,13 +116,28 @@ done:
     ret
 diskInfo ENDP
 
-; 64-bit divide: EDX:EAX / ECX = EAX (result)
-Div64 PROC
+; Divide 64-bit value (passed on stack) by bytesPerGB, result in EAX
+DivideByGB PROC
+    push ebp
+    mov ebp, esp
     push ebx
-    mov ebx, ecx        ; Divisor
-    div ebx             ; EDX:EAX / ECX
+
+    ; Get 64-bit value from stack (EDX:EAX)
+    mov eax, [ebp + 8]      ; Lower 32 bits
+    mov edx, [ebp + 12]     ; Upper 32 bits
+
+    ; Load divisor (1 GB)
+    mov ebx, DWORD PTR bytesPerGB ; Lower 32 bits of 1GB
+    ; bytesPerGB upper 32 bits are 0, so no need for full 64-bit division
+
+    ; Perform division: EDX:EAX / EBX
+    xor ecx, ecx            ; Clear ECX for simplicity
+    div ebx                 ; EDX:EAX / EBX, quotient in EAX, remainder in EDX
+
     pop ebx
-    ret
-Div64 ENDP
+    mov esp, ebp
+    pop ebp
+    ret 8                   ; Clean up 8 bytes (two DWORDs) from stack
+DivideByGB ENDP
 
 END
